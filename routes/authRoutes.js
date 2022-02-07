fdfd7e5b227fd84a46129d711a12bf594057de74
Ian <lsouza.ian@gmail.com>
2022-02-07 16:31:15 -0300

    create users + set up login/logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,5 +8,16 @@ module.exports = app => {
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+    res.redirect('/api/current_user')
+  });
+
+  app.get('/api/logout', (req, res) => {
+    req.logout(); // added automatically by passport
+    res.send(`Logged out. Current user: ${req.user}`);
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
